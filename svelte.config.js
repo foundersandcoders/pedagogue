597259992542958f21a27f6f77ef3c2878e9b78e
Jason Warren <jason@foundersandcoders.com>
2025-10-23 20:58:21 +0100

    build: rename package to "rhea"

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,9 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-// @type {import('@sveltejs/kit').Config}
 
 export default {
-  kit: { adapter: adapter() },
+  kit: {
+    adapter: adapter()
+  },
 	preprocess: vitePreprocess(),
 };
